da7bcb4a065236a44b2ba6bfcf010e23e57429ba
MariusDevelops <brazasmarius@gmail.com>
2024-10-02 21:52:42 +0300

    feat: complete create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { promises as fs } from "fs";
+
 const create = async () => {
-    // Write your code here 
+    const path = "src/fs/files/fresh.txt";
+    const content = "I am fresh and young";
+    const errMessage = "FS operation failed";
+
+    try {
+        await fs.access(path);
+        throw new Error(errMessage);
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            await fs.writeFile(path, content);
+        } else {
+            throw new Error(err.message);
+        }
+    }
 };
 
 await create();
