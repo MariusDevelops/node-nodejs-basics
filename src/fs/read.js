7c06a6afa33a8e08ee6b43854bb9c96c9f9394c7
MariusDevelops <brazasmarius@gmail.com>
2024-10-03 17:51:43 +0300

    feat: complete read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import * as fs from "node:fs/promises";
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = "src/fs/files/fileToRead.txt";
+    const errorMessage = "FS operation failed";
+
+    try {
+        await fs.access(pathToFile);
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
+
+    const contents = await fs.readFile(pathToFile, { encoding: "utf8" });
+    console.log(contents);
 };
 
 await read();
