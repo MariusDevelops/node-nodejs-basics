19a5875fac0341dbc742b71a8b5abe89ee68fbdf
MariusDevelops <brazasmarius@gmail.com>
2024-10-03 15:11:16 +0300

    feat: complete copy.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import * as fs from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+    const copyFrom = "src/fs/files/";
+    const copyTo = "src/fs/files_copy/";
+    const errorMessage = "FS operation failed";
+
+    try {
+        await fs.access(copyFrom);
+        try {
+            await fs.access(copyTo);
+            throw new Error(errorMessage);
+        } catch (error) {
+            if (error.code === "ENOENT") {
+                await fs.cp(copyFrom, copyTo, { recursive: true });
+            } else {
+                throw new Error(errorMessage);
+            }
+        }
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
 await copy();
