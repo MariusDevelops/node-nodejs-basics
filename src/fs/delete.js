d71452deb35b6df556595f63fed8bbd13ffb7155
MariusDevelops <brazasmarius@gmail.com>
2024-10-03 17:00:00 +0300

    feat: complete delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import * as fs from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+    const pathToRemoveFile = "src/fs/files/fileToRemove.txt";
+    const errorMessage = "FS operation failed";
+
+    try {
+        await fs.access(pathToRemoveFile);
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
+
+    await fs.unlink(pathToRemoveFile);
 };
 
 await remove();
