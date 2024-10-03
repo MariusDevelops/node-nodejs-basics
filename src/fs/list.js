2262e17a52e9bea31c28ddec16b78901a7c22912
MariusDevelops <brazasmarius@gmail.com>
2024-10-03 17:31:56 +0300

    feat: complete list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import * as fs from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+    const pathToFilesFolder = "src/fs/files/";
+    const errorMessage = "FS operation failed";
+
+    try {
+        await fs.access(pathToFilesFolder);
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
+
+    const files = await fs.readdir(pathToFilesFolder);
+
+    files.forEach((file) => {
+        console.log(file);
+    });
 };
 
 await list();
