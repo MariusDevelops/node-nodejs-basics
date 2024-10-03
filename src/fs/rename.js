d3b736a5bc3b8e51d40f1890ba21bbddbcb5e149
MariusDevelops <brazasmarius@gmail.com>
2024-10-03 16:04:20 +0300

    feat: complete rename.js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import * as fs from "node:fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+    const renamePathFrom = "src/fs/files/wrongFilename.txt";
+    const renamePathTo = "src/fs/files/properFilename.md";
+    const errorMessage = "FS operation failed";
+
+    try {
+        await fs.access(renamePathFrom);
+        try {
+            await fs.access(renamePathTo);
+            throw new Error(errorMessage);
+        } catch (error) {
+            if (error.code === "ENOENT") {
+                await fs.rename(renamePathFrom, renamePathTo);
+            } else {
+                throw new Error(errorMessage);
+            }
+        }
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
 await rename();
