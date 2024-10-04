7fcd4d86366f4e8f66933a7ece0d983c95319552
MariusDevelops <brazasmarius@gmail.com>
2024-10-04 18:01:32 +0300

    feat: complete env.js task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,22 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = {
+        RSS_host: "app.rs.school",
+        APP_host: "db.app.com",
+        RSS_port: "7070",
+        APP_port: "3030",
+        RSS_user: "user",
+        APP_user: "admin",
+    };
+
+    const envVariables = [];
+
+    Object.keys(env).forEach((key) => {
+        if (key.startsWith("RSS_")) {
+            envVariables.push(`${key}=${env[key]}`);
+        }
+    });
+
+    console.log(envVariables.join("; "));
 };
 
 parseEnv();
