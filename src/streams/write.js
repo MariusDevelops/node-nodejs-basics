b33cf411ba76df41cdc24f56b74e56481f923173
MariusDevelops <brazasmarius@gmail.com>
2024-10-06 20:34:12 +0300

    feat: complete streams/write.js task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from "node:fs";
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream("src/streams/files/fileToWrite.txt");
+
+    process.stdin.pipe(stream);
+
+    stream.on("finish", () => {
+        console.log("Text written successfully!");
+    });
 };
 
 await write();
