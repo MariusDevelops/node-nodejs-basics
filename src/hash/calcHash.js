ef8afd8671a0092f2cefd36bed20627a86586bbd
MariusDevelops <brazasmarius@gmail.com>
2024-10-06 16:47:55 +0300

    feat: complete calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+import { stdout } from "node:process";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash("sha256");
+
+    const input = createReadStream("src/hash/files/fileToCalculateHashFor.txt");
+    input.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
